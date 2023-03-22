9aed0ebdbabc36bb33cc6b3afc0a17025aac7b4b
José Miguel Reyes Márquez <miguel002004@hotmail.com>
2023-03-21 22:30:52 -0600

    register endpoint added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import "./lib/db";
+import * as fs from 'fs';
 import express from "express";
-import countryRoutes from "./routes/country";
+import { alfanumeric } from './generateID';
+
+const rawData = fs.readFileSync('./pcs.json');
+const data = JSON.parse(rawData.toString());
+//console.log(data)
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -10,10 +14,28 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+  res.json({ message: "si" });
 });
 
-app.use("/countries", countryRoutes);
+app.get('/users/:userId', (req, res) => {
+  const userId = req.params.userId;
+  res.send(`User ID: ${userId}`);
+});
+
+app.get("/register", async (req, res) => {
+  let id = alfanumeric(5);
+  if(id in data){
+    id = alfanumeric(5);
+  }
+  data[id] = {
+    "PcName": `nombre de pc${id}`,
+    "IdPc": id,
+    "publicURL": "publicurl.com"
+  };
+  // Guardar los cambios en el archivo JSON
+  fs.writeFileSync('./pcs.json', JSON.stringify(data));
+  res.json({ id: id });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
